b9227026cd0c03caafa0d2d319a9d1b52c7620d8
manuvaldes9 <manuelvaldes96@gmail.com>
2025-06-12 17:55:04 -0300

    RAG chain changes to RetrievalQA, better visualization on LangServe

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
 import os
 import openai
+from langchain_core.runnables import RunnableLambda
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 if not openai.api_key:
     raise RuntimeError("The variable OPENAI_API_KEY is not defined")
-    
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from langserve import add_routes
-from langchain_core.runnables import RunnableLambda
 from rag_chain import create_rag_chain, build_retriever
-from typing import Any
+
 
 class ChatInput(BaseModel):
-    question: str
+    question: str = Field(
+        "",
+        description="Ask about Promtior"
+    )
+
 
 app = FastAPI()
 
 retriever = build_retriever()
 rag_chain = create_rag_chain(retriever)
 
-
-# Función que procesa el chat manteniendo la estructura de Pydantic
-def chat_wrapper(inputs: Any) -> str:
-    # Extraer la pregunta del input
-    question = inputs.get("question") if isinstance(inputs, dict) else str(inputs)
-
-    # Crear state y ejecutar RAG
-    state = {"question": question}
-    result = rag_chain.invoke(state)
-    return result["answer"]
-
-
 answer_runnable = RunnableLambda(
-    chat_wrapper,
-    name="rag_chatbot"
-)
+    lambda inp: rag_chain.invoke(inp)["result"]
+).with_types(input_type=ChatInput)
 
 add_routes(
     app,
     answer_runnable,
     path="/chat",
-    input_type=ChatInput,
-    output_type=str,
     playground_type="default",
+    include_callback_events=False,
+    enable_feedback_endpoint=False,
+    enable_public_trace_link_endpoint=False,
 )
--- rag_chain.py
+++ rag_chain.py
@@ -1,62 +1,39 @@
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
-from langchain_core.documents import Document
-from typing_extensions import TypedDict, List
-from langgraph.graph import END, StateGraph
 
-# Estado tipado para LangChain
-class State(TypedDict, total=False):
-    question: str
-    context: List[Document]
-    answer: str
-
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-
-prompt = ChatPromptTemplate.from_messages([
-    ("Use the following context to respond the question of the user in a clear concise way"),
-    ("human", "Question: {question}\n\nContext:\n{context}")
-])
+def _prompt():
+    return ChatPromptTemplate.from_messages([
+        ("You are a useful Promtior assistant"),
+        ("You only can answer questions with the context you have"),
+        ("Use the context to respond the question of the user in a clear and concise way"),
+        ("If you don't have the answer in your context answer 'I don't have that information'"),
+        ("human", "Question: {question}\n\nContext:\n{context}")
+    ])
 
 def build_retriever():
     # Carga de documentos y preparacion
     loader = TextLoader("data/promtior_content.txt", encoding="utf-8")
     documents = loader.load()
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=100
+        chunk_size=500, chunk_overlap=100
     )
     docs = splitter.split_documents(documents)
-
     # Embeddings y vector store
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    vectorstore = FAISS.from_documents(docs, embeddings)
-
-    return vectorstore
+    vector_store = FAISS.from_documents(docs, embeddings)
+    return vector_store.as_retriever()
 
 def create_rag_chain(retriever):
     # Construir cadena RAG
-    def retrieve(state: State):
-        retrieved_docs = retriever.similarity_search(state["question"])
-        return {"question": state["question"], "context": retrieved_docs}
-
-
-    def generate(state: State):
-        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-        messages = prompt.invoke({"question": state["question"], "context": docs_content})
-        response = llm.invoke(messages)
-        return {"answer": response.content}
-
-    builder = StateGraph(State)
-    builder.add_node("retrieve", retrieve)
-    builder.add_node("generate", generate)
-    builder.set_entry_point("retrieve")
-    builder.add_edge("retrieve", "generate")
-    builder.add_edge("generate", END)
-    builder.set_finish_point("generate")
-    graph = builder.compile()
-
-    return graph
-
+    return RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+        chain_type="stuff",
+        retriever=retriever,
+        chain_type_kwargs={"prompt": _prompt()},
+        return_source_documents=False,
+        input_key="question"
+    )
